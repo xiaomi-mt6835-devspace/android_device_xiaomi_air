6ae7a4943717d2b3c76e34dd1816c0bce7b8d881
Matsvei Niaverau <matvej2002@gmail.com>
2022-08-15 17:36:58 +0300

    air: Import power HAL extension
    * Added fts_ts touchscreen support
    * From https://github.com/LineageOS/android_device_xiaomi_surya/blob/c28fc6e3ebbc8467858beac9b268c3ade9df1cad/power/power-mode.cpp
    * Adapted for our Mediatek power HAL
    
    Change-Id: I4fc9167720cd40e0489a75ec78a127f68bf2714a
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -152,6 +152,9 @@ BOARD_USES_METADATA_PARTITION := true
 # Platform
 TARGET_BOARD_PLATFORM := mt6835
 
+# Power
+TARGET_POWERHAL_MODE_EXT := $(DEVICE_PATH)/power/power-mode.cpp
+
 # Properties
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
