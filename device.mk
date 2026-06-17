335a6fef8942eacdcefe93ec3f6d3bc6cc6bb753
Raya Mahardika <94042378+rktdnt@users.noreply.github.com>
2026-06-17 19:55:02 +0700

    air: Import Public Libraries to tree

--- device.mk
+++ device.mk
@@ -252,6 +252,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
+# Public libraries
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
+
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors-service.xiaomi-multihal \
