17753e8c6c2b1664091723733f5d6d83b4c1b6f3
Raya Mahardika <94042378+rktdnt@users.noreply.github.com>
2026-06-20 05:48:51 +0000

    air: Back to stock media C2 services
    Change-Id: I89be1fd010b2240791965ab8e7fc3423420473dd

--- device.mk
+++ device.mk
@@ -192,9 +192,6 @@ PRODUCT_PACKAGES += \
     android.hardware.light-service.lineage
 
 # Media
-PRODUCT_PACKAGES += \
-    android.hardware.media.c2-mtk-service
-
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(DEVICE_PATH)/configs/media,$(TARGET_COPY_OUT_VENDOR)/etc)
 
